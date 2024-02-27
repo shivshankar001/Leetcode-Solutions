a04a85b44ccd68fd415714b8d9f571af098e41a6
Shivshankar Yadav <112503098+shivshankar001@users.noreply.github.com>
2024-02-27 19:05:02 +0530

    Time: 1038 ms (5.02%), Space: 627.7 MB (5.05%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
     int rec(int i, int j, string word1, string word2, vector<vector<int>> &dp)
     {
+        //base case
         if(i>=word1.size() || j>=word2.size())
         {
             return 0;
         }
-        
+        //cache check
         if(dp[i][j] != -1)
         {
            return dp[i][j]; 
         }
-        
+        //compute
         int ans = 0;
         ans = max(ans, rec(i+1, j, word1, word2, dp));
         ans = max(ans, rec(i, j+1, word1, word2, dp));
@@ -19,18 +20,19 @@ public:
         {
             ans = max(ans, 1+rec(i+1, j+1, word1, word2, dp));
         }
-        
+        //save and return
         return dp[i][j] = ans;
     }
     int minDistance(string word1, string word2) {
         int m = word1.size();
         int n = word2.size();
+        
         vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
         
         int temp = rec(0, 0, word1, word2, dp);
-        int ans = word1.size()+word2.size()-2*temp;
-        // ans += word2.size()-temp;
         
+        int ans = word1.size()+word2.size()-2*temp;
+
         return ans;
     }
 };
