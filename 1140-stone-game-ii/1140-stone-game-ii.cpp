087e38a33ca6d6b30a4dadeaf0d249afb4c397f8
Shivshankar Yadav <112503098+shivshankar001@users.noreply.github.com>
2024-03-01 01:26:49 +0530

    Time: 8 ms (88.76%), Space: 10.2 MB (77.75%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.cpp
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     
-    int dp[2][100][101], n, s[100];
+    int dp[100][101], n, s[100];
     vector<int> v;
     
-    int rec(int x, int y, int z)
+    int rec(int y, int z)
     {
         if (y >= n) return 0;
-        if (dp[x][y][z] != -1) return dp[x][y][z];
+        if (dp[y][z] != -1) return dp[y][z];
         int ans = 0, sum = 0;
         for(int i = y; i < n && i - y + 1 <= 2 * z; i++)
         {
             sum += v[i];
-            ans = max(ans, sum + (i + 1 < n ? s[i + 1] : 0) - rec(x ^ 1, i + 1, min(n, max(z, i - y + 1))));
+            ans = max(ans, sum + (i + 1 < n ? s[i + 1] : 0) - rec( i + 1, min(n, max(z, i - y + 1))));
         }
-        return dp[x][y][z] = ans;
+        return dp[y][z] = ans;
     }
     
     int stoneGameII(vector<int>& a) 
@@ -26,6 +26,6 @@ public:
         for(int i = n - 1; i >= 0; i--) x += a[i], s[i] = x;
         // for(int i=0;i<n;i++)
         //     cout<<s[i]<<" ";
-        return rec(1, 0, 1); // add
+        return rec(0, 1); // add
     }
 };
