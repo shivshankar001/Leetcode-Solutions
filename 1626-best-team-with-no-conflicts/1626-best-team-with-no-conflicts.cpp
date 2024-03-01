a103bdced52fceff4f18b9143fa91a0c16f924c6
Shivshankar Yadav <112503098+shivshankar001@users.noreply.github.com>
2024-03-01 11:40:57 +0530

    Time: 77 ms (60.84%), Space: 22.7 MB (68.00%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
@@ -1,64 +1,48 @@
 class Solution {
 public:
-//     int rec(int i, vector<pair<int, int>> &v)
-//     {
-// //         if(i>=v.size() || i<0)
-// //         {
-// //             return 0;
-// //         }
-        
-// //         int ans = v[i].second;
-// //         for(int j=0; j<i; j++)
-// //         {
-// //             if(v[i].second>=v[j].second)
-// //             {
-// //                 ans = max(ans, v[j].second+rec(j, v));   
-// //             }
-// //         }
-        
-// //         return ans;
-        
-        
-//     }
-    
-    int a[1000], n, dp[1000];
-    
-    int r(int x)
+    int rec(int i, vector<pair<int, int>> &v, vector<int> &dp)
     {
-        if (x >= n) return 0;
-        else if (dp[x] != -1) return dp[x];
-        int ans = 0;
-        for(int i = x + 1; i < n; i++)
+        // base check
+        if(i>=v.size() || i<0)
         {
-            if (a[i] >= a[x]) ans = max(ans, r(i));
+            return 0;
         }
-        return dp[x] = a[x] + ans;
+        //cache check
+        if(dp[i] != -1)
+        {
+            return dp[i];
+        }
+        //compute
+        int ans = v[i].second;
+        for(int j=0; j<i; j++)
+        {
+            if(v[i].second>=v[j].second)
+            {
+                ans = max(ans, v[i].second+rec(j, v, dp));   
+            }
+        }
+        //save and return
+        return dp[i]=ans;
+        
+        
     }
     
+    
     int bestTeamScore(vector<int>& scores, vector<int>& ages) {
-        memset(dp, -1, sizeof(dp));
+        
         vector<pair<int, int>> v;
         for(int i=0; i<scores.size(); i++)
         {
             v.push_back({ages[i], scores[i]});
         }
+        
         sort(v.begin(), v.end());
+        
+        vector<int> dp(ages.size(), -1);
+        
         int ans = 0;
-        n = ages.size();
-        for(int i = 0; i < n; i++) a[i] = v[i].second;
-        for(int i=0; i<n; i++) ans = max(ans, r(i));
+        for(int i=0; i<ages.size(); i++) ans = max(ans, rec(i, v, dp));
         return ans;
     }
     
-    // int r(int x)
-    // {
-    //     if (x >= n) return 0;
-    //     else if (dp[x] != -1) return dp[x];
-    //     int ans = 0;
-    //     for(int i = x + 1; i < n; i++)
-    //     {
-    //         if (a[i] >= a[x]) ans = max(ans, rec(i));
-    //     }
-    //     return dp[x] = a[x] + ans;
-    // }
 };
