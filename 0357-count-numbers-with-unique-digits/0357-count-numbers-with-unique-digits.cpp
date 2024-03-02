b5a7953df8eaeca697815c35f7a21014cbd1afc0
Shivshankar Yadav <112503098+shivshankar001@users.noreply.github.com>
2024-03-02 12:43:03 +0530

    Time: 3 ms (19.89%), Space: 7.2 MB (24.32%) - LeetHub

--- 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.cpp
+++ 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.cpp
@@ -4,11 +4,22 @@ public:
         if(n<=1) return 1;
         else return n*fact(n-1);
     }
-    int rec(int n) {
-        if(n==0) return 1;
-        else return (rec(n-1) + (9* (fact(9)/fact(10-n))));
+    int rec(int n, vector<int> &dp) {
+        if(n==0)
+        {
+            return 1;
+        }
+        else 
+        {
+            if(dp[n] != -1)
+            {
+                return dp[n];
+            }
+            return dp[n] = (rec(n-1, dp) + (9* (fact(9)/fact(10-n))));
+        }
     }
     int countNumbersWithUniqueDigits(int n) {
-        return rec(n);
+        vector<int> dp(n+1, -1);
+        return rec(n, dp);
     }
 };
