5b4f02c6d001ed70916edf28504a04ce0d4fb339
Shivshankar Yadav <112503098+shivshankar001@users.noreply.github.com>
2024-03-25 00:07:48 +0530

    Time: 302 ms (5.14%), Space: 61.6 MB (59.14%) - LeetHub

---
+++ 0954-array-of-doubled-pairs/0954-array-of-doubled-pairs.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    bool canReorderDoubled(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        
+        int cnt0 = 0;
+        unordered_map<int, int> mp;
+        for(int i=0; i<arr.size(); i++)
+        {
+            if(arr[i] == 0)
+            {
+                cnt0++;
+            }
+            mp[arr[i]]++;
+        }
+        if(cnt0%2 != 0)
+        {
+            return false;
+        }
+        
+        int cnt = 0;
+        for(int i=0; i<arr.size(); i++)
+        {
+            int temp = arr[i]*2;
+            if(mp.find(temp) == mp.end())
+            {
+                // return false;
+                // cout<<"bol"<<endl;
+            }
+            else
+            {
+                cout<<"temp : "<<temp<<endl;
+                cout<<mp[temp]-1<<endl;
+                if(mp[arr[i]]-1 >= 0 && mp[temp]-1 >= 0)
+                {
+                    mp[arr[i]]--;
+                    mp[temp]--;
+                    cnt++;
+                    cout<<"bol"<<endl;
+                }
+            }
+        }
+        cout<<cnt;
+        if(cnt >= arr.size()/2)
+            return true;
+        return false;
+    }
+};
