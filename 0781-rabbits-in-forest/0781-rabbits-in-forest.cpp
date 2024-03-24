9aa21de14b554457249a464614a5a8123befda0b
Shivshankar Yadav <112503098+shivshankar001@users.noreply.github.com>
2024-03-24 12:57:17 +0530

    Time: 9 ms (5.32%), Space: 12.6 MB (11.14%) - LeetHub

---
+++ 0781-rabbits-in-forest/0781-rabbits-in-forest.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int numRabbits(vector<int>& answers) {
+        int n = answers.size();
+        unordered_map<int, int> mp;
+        set<int> s;
+        for(int i=0; i<n; i++)
+        {
+            mp[answers[i]]++;
+            s.insert(answers[i]);
+        }
+        // cout<<s.size();
+        int ans = 0;
+        for(auto it : s)
+        {
+            // if(it+1 <= mp[it])
+            // {
+            //     if(mp[it]%(it+1) == 0)
+            //     {
+            //         ans += (mp[it]/(it+1))*3;
+            //     }
+            //     else
+            //     {
+            //         int temp = (mp[it]/(it+1))+1;
+            //         ans += (temp*3);
+            //     }
+            // }
+            if(it == 0)
+            {
+                ans += mp[0];
+            }
+            else
+            {
+                if(mp[it] >= (it+1))
+                {
+                    if(mp[it]%(it+1) == 0)
+                    {
+                        ans += (mp[it]/(it+1))*(it+1);
+                    }
+                    else
+                    {
+                        int temp = (mp[it]/(it+1))+1;
+                        ans += (temp*(it+1));
+                    }
+                }
+                else
+                {
+                    ans += (it+1);
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
