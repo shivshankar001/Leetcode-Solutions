4f1b42c9ba8c4ddde7aa7b8a1f473cc1ac65874e
Shivshankar Yadav <112503098+shivshankar001@users.noreply.github.com>
2024-04-01 14:52:03 +0530

    Time: 23 ms (53.90%), Space: 25.2 MB (23.38%) - LeetHub

--- 1090-largest-values-from-labels/1090-largest-values-from-labels.cpp
+++ 1090-largest-values-from-labels/1090-largest-values-from-labels.cpp
@@ -2,42 +2,35 @@ class Solution {
 public:
     int largestValsFromLabels(vector<int>& values, vector<int>& labels, int numWanted, int useLimit) {
         int n = values.size();
+        
         vector<pair<int, int>> v;
         for(int i=0; i<n; i++)
         {
             v.push_back({values[i], labels[i]});
         }
+        
         sort(v.begin(), v.end());
-        // for(int i=0; i<n; i++)
-        // {
-        //     v.push_back({values[i], labels[i]});
-        // }
-        int limit = useLimit;
-        // int temp = -1;
+        
         int sum = 0, ans = 0;
         unordered_map<int, int> mp, mp1;
         for(int i=n-1; i>=0; i--)
         {
-            int temp = v[i].second;
-            if(mp.find(temp) == mp.end())
+            if(mp.find(v[i].second) == mp.end())
             {
                 sum += v[i].first;
                 ans = max(ans, sum);
-                mp[temp]++;
-                mp1[temp] = useLimit - 1;
+                mp[v[i].second]++;
+                mp1[v[i].second] = useLimit - 1;
                 numWanted--;
             }
-            else if(mp[temp] > 0 && mp1[temp] > 0)
+            else if(mp[v[i].second] > 0 && mp1[v[i].second] > 0)
             {
-                // cout<<"bol"<<endl;
                 sum += v[i].first;
                 ans = max(ans, sum);
-                // temp = v[i].second;
-                mp1[temp] -= 1;
+                mp1[v[i].second] -= 1;
                 numWanted--;
             }
-            // cout<<temp<<endl;
-            // cout<<sum<<" "<<ans<<endl;
+
             if(numWanted<=0)
             {
                 break;
