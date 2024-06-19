12b98e631aa657d32febec4d0b67d06757b29db1
Shivshankar Yadav <112503098+shivshankar001@users.noreply.github.com>
2024-06-19 19:34:54 +0530

    Time: 358 ms (5.90%), Space: 125.3 MB (97.64%) - LeetHub

--- 3152-special-array-ii/3152-special-array-ii.cpp
+++ 3152-special-array-ii/3152-special-array-ii.cpp
@@ -4,18 +4,12 @@ public:
         vector<int> v;
         for(int i=0; i<nums.size()-1; i++)
         {
-            if((nums[i]%2==0 && nums[i+1]%2==0) || (nums[i]%2!=0 && nums[i+1]%2!=0))
+            if(nums[i]%2 == nums[i+1]%2)
             {
                 v.push_back(i);
             }
         }
-        
-        // for(int i=0; i<v.size(); i++)
-        // {
-        //     cout<<v[i]<<" ";
-        // }
-        // cout<<endl;
-        
+
         vector<bool> ans;
         for(int i=0; i<queries.size(); i++)
         {
