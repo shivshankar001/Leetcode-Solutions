bfcf9baa8c8886de07b960405cae4293875596d1
Shivshankar Yadav <112503098+shivshankar001@users.noreply.github.com>
2024-06-27 00:07:16 +0530

    Time: 21 ms (29.30%), Space: 19.4 MB (11.38%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -14,21 +14,16 @@ public:
         if(dp[i][k] != -1)
             return dp[i][k];
         
-        // vis[i] = true;
         int x = 1e7;
-        // if(!adj[i].empty()) x+=adj[i].second;
         for(auto it : adj[i])
         {
-            // if(!vis[it.first])
-            // {
-            //     x = min(x, it.second+rec(adj, it.first, dst, n, k-1, vis));
-            // }
             x = min(x, it.second+rec(adj, it.first, dst, n, k-1, dp));
         }
         
         return dp[i][k] = x;
     }
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        
         vector<pair<int, int>> adj[n];
         for(int i=0; i<flights.size(); i++)
         {
@@ -36,17 +31,10 @@ public:
             adj[fr].push_back({to, pr});
         }
         
-        // for(auto it : adj[3])
-        // {
-        //     cout<<it.first<<"   "<<it.second<<endl;
-        // }
-        
         vector<vector<int>> dp(101, vector<int>(101, -1));
-        vector<bool> vis(n, false);
         int ans = rec(adj, src, dst, n, k+1, dp);
         
         if(ans == 1e7) return -1;
-        
         return ans;
     }
 };
