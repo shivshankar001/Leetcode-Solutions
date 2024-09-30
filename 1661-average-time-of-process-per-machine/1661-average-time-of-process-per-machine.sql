cb40c9d550afd27fbbed1b904001bcda813e4178
Shivshankar Yadav <112503098+shivshankar001@users.noreply.github.com>
2024-09-30 19:28:53 +0530

    Time: 257 ms (19.17%), Space: 0B (100.00%) - LeetHub

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -4,5 +4,7 @@ FROM Activity AS a1
 JOIN Activity AS a2
 ON a1.machine_id = a2.machine_id
 AND a1.process_id = a2.process_id
-AND a1.timestamp < a2.timestamp
+# AND a1.timestamp < a2.timestamp
+AND a1.activity_type = 'start'
+AND a2.activity_type = 'end'
 GROUP BY a1.machine_id;
