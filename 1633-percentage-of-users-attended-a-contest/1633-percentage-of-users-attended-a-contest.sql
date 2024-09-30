7c061b9a7baeddac54845c61643a6ee76ddbd400
Shivshankar Yadav <112503098+shivshankar001@users.noreply.github.com>
2024-09-30 23:06:06 +0530

    Time: 877 ms (79.35%), Space: 0B (100.00%) - LeetHub

---
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,6 @@
+SELECT r.contest_id, ROUND((COUNT(r.user_id) * 100 / (SELECT COUNT(*) FROM Users)), 2) AS percentage
+FROM Register AS r
+LEFT JOIN Users AS u
+ON r.user_id = u.user_id
+GROUP BY r.contest_id
+ORDER BY percentage DESC, r.contest_id ASC;
