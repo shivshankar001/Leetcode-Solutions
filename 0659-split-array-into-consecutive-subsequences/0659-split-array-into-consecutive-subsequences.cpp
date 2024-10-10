526b6bc8c56ccd10c9339a7c6d3e8f0bc62c54b5
Shivshankar Yadav <112503098+shivshankar001@users.noreply.github.com>
2024-10-11 02:29:24 +0530

    Time: 55 ms (91.91%), Space: 58.4 MB (94.78%) - LeetHub

--- 0659-split-array-into-consecutive-subsequences/0659-split-array-into-consecutive-subsequences.cpp
+++ 0659-split-array-into-consecutive-subsequences/0659-split-array-into-consecutive-subsequences.cpp
@@ -1,16 +1,20 @@
 using ii = pair<int, int>;
 class Solution {
 public:
-    class comparasion {
+    class comp{
         public:
-        bool operator()(pair<int, int>& a, pair<int, int>& b) {//always put the smallest and shortest sequence on the top of the heap
-            if(a.first == b.first) return a.second > b.second;
-            return a.first > b.first;
-        };
+        bool operator()(ii &a, ii &b)
+        {
+            if(a.first==b.first)
+            {
+                return a.second>b.second;
+            }
+            return a.first>b.first;
+        }
     };
     bool isPossible(vector<int>& a) {
         int n = a.size();
-        priority_queue<ii, vector<ii>, comparasion> pq;
+        priority_queue<ii, vector<ii>, comp> pq;
         for(int i=0; i<n; i++)
         {
             if(pq.empty())
@@ -19,8 +23,6 @@ public:
             }
             else
             {
-                cout<<"i : "<<i<<endl;
-                cout<<pq.top().first<<" "<<pq.top().second<<endl;
                 if(pq.top().first == a[i])
                 {
                     pq.push({a[i], 1});
@@ -28,10 +30,7 @@ public:
                 else if(pq.top().first+1 == a[i])
                 {
                     auto it = pq.top();
-                    cout<<"here"<<endl;
-                    // cout<<it.first<<" "<<it.second<<endl;
                     pq.pop();
-                    cout<<"pushing : "<<a[i]<<" "<<it.second+1<<endl;
                     pq.push({a[i], it.second+1});
                 }
                 else
@@ -47,7 +46,6 @@ public:
         }
         while(!pq.empty())
         {
-            cout<<pq.top().second<<endl;
             if(pq.top().second<3)
             {
                 return false;
