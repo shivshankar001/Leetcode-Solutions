646030160d579f56b4d2b2c42f4cf4022fed59d9
Shivshankar Yadav <112503098+shivshankar001@users.noreply.github.com>
2024-10-11 02:45:11 +0530

    Time: 193 ms (59.59%), Space: 141 MB (52.35%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -1,36 +1,33 @@
+using ii = pair<int, int>;
 class Solution {
 public:
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        int m = nums1.size();
-        int n = nums2.size();
-
+        int n1 = nums1.size();
+        int n2 = nums2.size();
         vector<vector<int>> ans;
-        set<pair<int, int>> visited;
-
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>,
-                       greater<pair<int, pair<int, int>>>> minHeap;
-        minHeap.push({nums1[0] + nums2[0], {0, 0}});
-        visited.insert({0, 0});
-
-        while (k-- && !minHeap.empty()) {
-            auto top = minHeap.top();
-            minHeap.pop();
-            int i = top.second.first;
-            int j = top.second.second;
-
+        priority_queue<pair<int, ii>, vector<pair<int, ii>>, greater<pair<int, ii>>> pq;
+        pq.push({nums1[0]+nums2[0], {0, 0}});
+        set<ii> st;
+        st.insert({0, 0});
+        while(!pq.empty() && k--)
+        {
+            auto it = pq.top();
+            pq.pop();
+            int sum = it.first;
+            int i = it.second.first;
+            int j = it.second.second;
             ans.push_back({nums1[i], nums2[j]});
-
-            if (i + 1 < m && visited.find({i + 1, j}) == visited.end()) {
-                minHeap.push({nums1[i + 1] + nums2[j], {i + 1, j}});
-                visited.insert({i + 1, j});
+            if(i+1<n1 && st.find({i+1, j})==st.end())
+            {
+                pq.push({nums1[i+1]+nums2[j], {i+1, j}});
+                st.insert({i+1, j});
             }
-
-            if (j + 1 < n && visited.find({i, j + 1}) == visited.end()) {
-                minHeap.push({nums1[i] + nums2[j + 1], {i, j + 1}});
-                visited.insert({i, j + 1});
+            if(j+1<n2 && st.find({i, j+1})==st.end())
+            {
+                pq.push({nums1[i]+nums2[j+1], {i, j+1}});
+                st.insert({i, j+1});
             }
         }
-
         return ans;
     }
 };
